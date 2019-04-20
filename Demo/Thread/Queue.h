3a6166ae52da64a6a21544ef6c3938d04105b55f
luojiantao <33904593+luojiantao@users.noreply.github.com>
2019-04-20 16:45:27 +0800

    Update Queue.h

--- Demo/Thread/Queue.h
+++ Demo/Thread/Queue.h
@@ -2,6 +2,8 @@
 #include "task.h"
 #include <list>
 #include <atomic>
+#include <mutex>
+#include <memory>
 /*
 enum Type{
 	    Io = 0,
@@ -23,14 +25,32 @@ struct TaskAttr{
 namespace ThreadPoolDemo{
 
 struct Node{
-    Node():next(nullptr), task(nullptr)
-    {}
-    Node(std::function<void()>&& func):next(nullptr), task(func)
-    {}
-    ~Node(){}
+    Node():next(nullptr), task([](){return;})
+    {
+      //  std::unique_lock<std::mutex> lk(m_mtx);
+    }
+    Node(std::function<void()>& func)
+    {
+        //std::unique_lock<std::mutex> lk(m_mtx);
+	task = func;
+	next = nullptr;
+    }
+    void load(std::function<void()>& func){
+        //std::unique_lock<std::mutex> lk(m_mtx);
+	func = task; 
+    }
+    void link_tail(Node* n){
+        next = n;
+    }
+    ~Node(){
+	//next = nullptr;
+    }
     std::function<void()> task;
     Node* next;
+    //std::atomic<Node*> next; 
     int value = 0;
+    //std::atomic_flag// TODO
+    std::mutex m_mtx;
     char mm[100];
 };
 
@@ -43,15 +63,19 @@ public:
         delete m_head.load();
 	m_head = nullptr;
     }
-    void push_back(int v);
+    //void push_back(int v);
+    void push_back(std::function<void()> task);
 
-    bool pop_front(int& task);
+    //bool pop_front(int& task);
+    bool pop_front(std::function<void()>& task);
+    
+    bool pop_front(std::shared_ptr<std::function<void()>>& task);
 
     void print();
     bool empty();
 
     void print_ptr(){
-       std::cout << "EOF: " << m_Eof->value << std::endl;
+     //  std::cout << "EOF: " << m_Eof->value << std::endl;
        std::cout << "head: " << (m_head.load())->value << std::endl;
        std::cout << "tail: " << (m_tail.load())->value << std::endl;
     }
@@ -62,7 +86,7 @@ private:
     Priority m_pri;
     std::atomic<Node*> m_head;
     std::atomic<Node*> m_tail;
-    Node* m_Eof;
+    //Node* m_Eof;
 };
 
 }
