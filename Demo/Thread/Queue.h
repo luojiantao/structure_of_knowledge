9f24220109487129524c771c134552e78890c086
luojiantao <33904593+luojiantao@users.noreply.github.com>
2019-04-28 18:57:23 +0800

    Update Queue.h

--- Demo/Thread/Queue.h
+++ Demo/Thread/Queue.h
@@ -1,9 +1,14 @@
 #pragma once
-#include "task.h"
+//#include "task.h"
+//#include "blockingconcurrentqueue.h"
+#include "concurrentqueue.h"
 #include <list>
 #include <atomic>
 #include <mutex>
 #include <memory>
+#include <unistd.h>
+#include <iostream>
+#include "BaseStruct.h"
 /*
 enum Type{
 	    Io = 0,
@@ -20,85 +25,72 @@ struct TaskAttr{
 	        Priority proprity = Priority::Normal;
 		    int time = 0;
 };
-*/
 
+*/
 namespace ThreadPoolDemo{
 
 struct Node{
-    Node():next(nullptr), task([](){return;})
-    {
-      //  std::unique_lock<std::mutex> lk(m_mtx);
-    }
-    Node(std::function<void()>& func)
-    {
-        //std::unique_lock<std::mutex> lk(m_mtx);
-	task = func;
-	next = nullptr;
-    }
-    void load(std::function<void()>& func){
-        //std::unique_lock<std::mutex> lk(m_mtx);
-	func = task; 
-	//func = [](){}; 
-    }
-    void link_tail(Node* n){
-        next = n;
-    }
-    ~Node(){
-	//next = nullptr;
-    }
+	Node(){}
+    Node(std::function<void()>& t):task(t){}
     std::function<void()> task;
-    //Node* next;
-    std::atomic<Node*> next; 
-    int value = 0;
-    //std::atomic_flag// TODO
-    std::mutex m_mtx;
-    char mm[100];
 };
 
-class CNodeLink{
-private:
-    Node* m_node;
-    Node* m_next;
-};
 class CPriqueue{
 public:
-    CPriqueue(){
+    CPriqueue(Type type = Type::Io, Priority pri = Priority::Normal):m_type(type), m_pri(pri){
         init();
     }
     ~CPriqueue(){
-        delete m_head.load();
-	m_head = nullptr;
     }
-    //void push_back(int v);
     void push_back(std::function<void()> task);
-
-    //bool pop_front(int& task);
     bool pop_front(std::function<void()>& task);
     
     bool pop_front(std::shared_ptr<std::function<void()>>& task);
 
-    void print();
     bool empty();
-
-    void print_ptr(){
-     //  std::cout << "EOF: " << m_Eof->value << std::endl;
-       std::cout << "head: " << (m_head.load())->value << std::endl;
-       std::cout << "tail: " << (m_tail.load())->value << std::endl;
-    }
-
+    void print();
 private:
     void init();
     Type m_type;
     Priority m_pri;
-    //std::atomic<Node*> m_head;
-    std::atomic<Node*> m_tail;
-    struct Head{
-        std::atomic<Node*> head;
-        std::atomic<int> count;
-    };
-
-    Head m_head1;
-    //Node* m_Eof;
+    moodycamel::ConcurrentQueue<Node> m_q;
 };
 
+class CTimingQueue{
+public:
+struct TimingNode{
+   int delay = 0;
+   bool once = false;
+   std::function<void()> task;
+};
+    void push_back(const std::function<void()>& task, int time = 0, bool once = false);
+	
+    void push_back(const TimingNode& node);
+	
+    bool pop_front(std::function<void()>& task);
+	
+private:
+    moodycamel::ConcurrentQueue<TimingNode> m_q;
+};
+
+class CPriqueueManager{
+public:
+    CPriqueueManager():m_AdvancedQueue(Type::Io, Priority::Advanced), m_NormalQueue(Type::Io, Priority::Normal)
+    {
+    }
+    ~CPriqueueManager(){}
+	
+	void push_task(const std::function<void()>& task, TaskAttr attr);
+	
+    bool pop_task(std::function<void()> &task, Priority pri, Type type);
+	
+    bool empty(Type type);
+private:
+   CPriqueue m_AdvancedQueue;
+   CPriqueue m_NormalQueue;
+   CPriqueue m_CalculateQueue;
+   CTimingQueue m_TimingQueue;
+   CPriqueueManager(const CPriqueueManager&) = delete;
+   CPriqueueManager& operator=(const CPriqueueManager&) = delete;
+};
 }
